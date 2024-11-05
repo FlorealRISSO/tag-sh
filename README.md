c0614b93c9565dabb8881e251dd2ab08ed1f25bf
Floreal Risso <floreal.risso@univ-tlse3.fr>
2024-11-05 21:19:05 +0100

    add support multi volumes

--- README.md
+++ README.md
@@ -21,6 +21,14 @@ This script provides a simple tagging system for files on Linux. It allows you t
    chmod +x tag.sh
    ```
 
+3. **Set TAGS_DIRS Variable**
+
+   Before using the script, set the `TAGS_DIRS` variable to include all desired volumes for tagging. For example:
+
+   ```sh
+   export TAGS_DIRS="$HOME:/media/usb1"
+   ```
+
 ## Usage
 
 ### Add a Tag to a File
@@ -60,32 +68,32 @@ This command lists all files that are tagged with `work`.
 To remove a tag from a file, use the following command:
 
 ```sh
-./tag.sh rm <TAG_NAME> <FILE>
+./tag.sh rm <TAG_NAME> <VOLUME> <FILE>
 ```
 
 **Example:**
 
 ```sh
-./tag.sh rm work project.txt
+./tag.sh rm work /media/usb1/project.txt
 ```
 
-This command removes the tag `work` from the file `project.txt`.
+This command removes the tag `work` from the file `project.txt` located on the specified volume.
 
 ### Get the Relative Path of a Tagged File
 
 To get the path of a file associated with a tag, use the following command:
 
 ```sh
-./tag.sh get <TAG_NAME> <FILE>
+./tag.sh get <TAG_NAME> <VOLUME> <FILE>
 ```
 
 **Example:**
 
 ```sh
-./tag.sh get work project.txt
+./tag.sh get work /media/usb1/project.txt
 ```
 
-This command prints the path to `project.txt`,  which would be something like `$HOME/.tags/work/project.txt`.
+This command prints the path to `project.txt`, which would be something like `$HOME/.tags/work/project.txt`.
 
 ## Error Handling
 
@@ -93,6 +101,7 @@ This command prints the path to `project.txt`,  which would be something like `$
 - **Tag Directory Does Not Exist:** If the specified tag does not exist when listing, removing, or getting, an error message will be displayed.
 - **File Already Tagged:** When adding a tag, if the file is already tagged with the same tag, an error message will be displayed.
 - **File Not Tagged:** When removing or getting a tag, if the file is not tagged with the specified tag, an error message will be displayed.
+- **Volume Not Taggable:** If the specified volume is not taggable, an error message will be displayed.
 
 ## Requirements
 
--- tag.sh
+++ tag.sh
@@ -1,41 +1,68 @@
 #!/bin/sh
 
 # Directory to store tags
-TAGS_DIR=${TAGS_DIR:-$HOME/.tags}
+TAGS_DIRS="$HOME"
+TAGS_DIR=".tags"
+
+get_mountpoint() {
+	df -P "$1" | awk 'NR==2 {print $6}'
+}
+
+is_taggable() {
+	local_file="$1"
+	IFS=':'
+	for dir in $TAGS_DIRS; do
+		if [ "$dir" = "$local_file" ]; then
+			return 0
+		fi
+	done
+	return 1
+}
 
 die() { echo "$*" >&2; exit 1; }
 
 add_tag() {
 	tag_name=$1
 	file=$2
-	tag_dir=$TAGS_DIR/$tag_name
 	tag_file_basename=$(basename "$file")
 
 	[ ! -f "$file" ] && die "Error: File '$file' does not exist."
 
-	mkdir -p "$TAGS_DIR"
+	mountpoint=$(get_mountpoint "$file")
+	[ -z "$mountpoint" ] && die "Error: Could not determine mountpoint for '$file'."
+	is_taggable "$mountpoint" || die "Error: '$mountpoint' is not taggable."
+
+	tag_dir="$mountpoint/$TAGS_DIR/$tag_name"
 	mkdir -p "$tag_dir"
 
 	[ -f "$tag_dir/$tag_file_basename" ] &&
 		die "Error: File '$file' is already tagged with '$tag_name'."
 
-	ln "$file" "$tag_dir/$tag_file_basename"
+ 	
+	(cd "$tag_dir"; ln "$file" "$tag_file_basename")
 	echo "Tagged '$file' with '$tag_name'"
 }
 
 list_tag() {
 	tag_name=$1
-	tag_dir=$TAGS_DIR/$tag_name
-	[ ! -d "$tag_dir" ] && die "Tag '$tag_name' does not exist."
-	ls -1 "$tag_dir" | awk 'NF'
+
+	IFS=':'
+	for dir in $TAGS_DIRS; do
+		tag_dir="$dir/$TAGS_DIR/$tag_name"
+		if [ -d "$tag_dir" ]; then
+			ls -1 "$tag_dir" | awk -v dir=$dir '{print dir,$0}'
+		fi
+	done
 }
 
 rm_tag() {
 	tag_name=$1
-	file=$2
-	tag_dir=$TAGS_DIR/$tag_name
+	dir=$2
+	file=$3
 	tag_file_basename=$(basename "$file")
+	tag_dir="$dir/$TAGS_DIR/$tag_name"
 
+	is_taggable "$dir" || die "Error: '$dir' is not taggable".
 	[ ! -d "$tag_dir" ] && die "Tag '$tag_name' does not exist."
 	[ ! -f "$tag_dir/$tag_file_basename" ] &&
 		die "Error: File '$file' is not tagged with '$tag_name'."
@@ -46,10 +73,12 @@ rm_tag() {
 
 get_tag() {
 	tag_name=$1
-	file=$2
-	tag_dir=$TAGS_DIR/$tag_name
+	dir=$2
+	file=$3
 	tag_file_basename=$(basename "$file")
+	tag_dir="$dir/$TAGS_DIR/$tag_name"
 
+	is_taggable "$dir" || die "Error: '$dir' is not taggable".
 	[ ! -d "$tag_dir" ] && die "Tag '$tag_name' does not exist."
 	[ ! -f "$tag_dir/$tag_file_basename" ] &&
 		die "Error: File '$file' is not tagged with '$tag_name'."
@@ -71,24 +100,24 @@ list)
 	list_tag "$2"
 	;;
 rm)
-	if [ -z "$2" ] || [ -z "$3" ]; then
-		die "Usage: $0 rm <TAG_NAME> <FILE>"
+	if [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+		die "Usage: $0 rm <TAG_NAME> <VOLUME> <FILE>"
 	fi
-	rm_tag "$2" "$3"
+	rm_tag "$2" "$3" "$4"
 	;;
 get)
-	if [ -z "$2" ] || [ -z "$3" ]; then
-		die "Usage: $0 get <TAG_NAME> <FILE>"
+	if [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+		die "Usage: $0 get <TAG_NAME> <VOLUME> <FILE>"
 	fi
-	get_tag "$2" "$3"
+	get_tag "$2" "$3" "$4"
 	;;
 *)
 	progname=$(basename "$0")
 	{
 		echo "Usage: $progname add <TAG_NAME> <FILE>"
 		echo "       $progname list <TAG_NAME>"
-		echo "       $progname rm <TAG_NAME> <FILE>"
-		echo "       $progname get <TAG_NAME> <FILE>"
+		echo "       $progname rm <TAG_NAME> <VOLUME> <FILE>"
+		echo "       $progname get <TAG_NAME> <VOLUME> <FILE>"
 	} >&2
 	exit 1
 	;;
